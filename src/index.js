426f8f929372305f4f6bd9a615cf78adf5614dd0
aldo1400 <achoque1400@gmail.com>
2020-05-10 22:27:45 -0500

    webpack config

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./styles/index.css";
+import img from "./assets/fondo.jpg";
+
+const element = document.getElementById("fondo");
+element.src = img;
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    port: "5005",
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /.(js)$/,
+        use: ["babel-loader"],
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    new MiniCssExtractPlugin({ filename: "bundle.css" }),
+  ],
+};
